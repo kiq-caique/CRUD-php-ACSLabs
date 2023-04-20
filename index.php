4100153669f03f88d341b6239b920b87bb3c4dc2
kiq-caique <caique-kiq@hotmail.com>
2023-04-20 14:24:16 -0300

    CRUD-PHP

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+include('conexao.php');
+require __DIR__.'/vendor/autoload.php';
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/footer.php';
+
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM users WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: paginainicial.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="d-flex justify-content-center">
+        <h1>Acesse sua conta</h1>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <form action="" method="post">
+            <p>
+                <label>E-mail</label>
+                <input type="text" name="email">
+            </p>
+            <p>
+                <label>Senha</label>
+                <input type="password" name="senha">
+            </p>
+            <p>
+                <button type="submit" class="btn btn-success">Entrar</button>
+            </p>
+        </form>
+    </div>
+</body>    
+</html>
